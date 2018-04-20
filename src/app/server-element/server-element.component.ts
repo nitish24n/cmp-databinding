0033930ffe0325da94f0a4a3c7fb2e39963844db
nitish24n <nitish24m@gmail.com>
2018-04-20 14:32:03 +0530

    Binding to custom properties

--- src/app/server-element/server-element.component.ts
+++ src/app/server-element/server-element.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit , Input } from '@angular/core';
+import { element } from 'protractor';
 
 @Component({
   selector: 'app-server-element',
@@ -6,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./server-element.component.css']
 })
 export class ServerElementComponent implements OnInit {
-
+  @Input() element: {  type: string, name: string, content: string  };
   constructor() { }
 
   ngOnInit() {
