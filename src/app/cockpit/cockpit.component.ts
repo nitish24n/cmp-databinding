a671671b1e346de79cc231933f0f0f78731d4198
nitish24n <nitish24m@gmail.com>
2018-04-20 20:23:02 +0530

    Assigning an alias to custom events

--- src/app/cockpit/cockpit.component.ts
+++ src/app/cockpit/cockpit.component.ts
@@ -12,7 +12,7 @@ export class CockpitComponent implements OnInit {
   ngOnInit() {
   }
   @Output() serverCreated = new EventEmitter<{ serverName: string, serverContent: string }>();
-  @Output() blueprintCreated = new EventEmitter<{ serverName: string, serverContent: string }>();
+  @Output('bpCreated') blueprintCreated = new EventEmitter<{ serverName: string, serverContent: string }>();
   newServerName = '';
   newServerContent = '';
 
